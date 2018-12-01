dd8593d036958672cb3c5f25a81ad4f44406996f
Erkan Tomruk <erkantomruk@gmail.com>
2018-12-01 21:30:00 +0300

    Add face detection & emotion analysis

--- app/src/main/java/com/smalldata/rater/PhotoTakerActivity.java
+++ app/src/main/java/com/smalldata/rater/PhotoTakerActivity.java
@@ -1,12 +1,19 @@
 package com.smalldata.rater;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.icu.text.SimpleDateFormat;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -27,13 +34,28 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Date;
 
+
+import com.microsoft.projectoxford.face.*;
+import com.microsoft.projectoxford.face.contract.*;
 
 public class PhotoTakerActivity extends AppCompatActivity {
     RequestQueue requestQueue;
     String apiURL ="http://www.google.com";
+    private ProgressDialog detectionProgressDialog;
+
+    private final String apiEndpoint = "https://westeurope.api.cognitive.microsoft.com/face/v1.0";
+
+    private final String subscriptionKey = "139aa74fda7c4448b2328ddc723e4934";
+
+    private final FaceServiceClient faceServiceClient =
+            new FaceServiceRestClient(apiEndpoint, subscriptionKey);
 
     private Uri file;
 
@@ -47,6 +69,8 @@ public class PhotoTakerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        detectionProgressDialog = new ProgressDialog(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_taker);
 
@@ -107,7 +131,12 @@ public class PhotoTakerActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 StringRequest request = prepRequest();
                 requestQueue.add(request);
-                imageView.setImageURI(file);
+                //imageView.setImageURI(file);
+                try {
+                    detectAndFrame(MediaStore.Images.Media.getBitmap(this.getContentResolver(), file));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
@@ -130,5 +159,116 @@ public class PhotoTakerActivity extends AppCompatActivity {
     }
 
     public void endTravel(View view) { }
+
+    // Detect faces by uploading a face image.
+// Frame faces after detection.
+    private void detectAndFrame(final Bitmap imageBitmap) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+        ByteArrayInputStream inputStream =
+                new ByteArrayInputStream(outputStream.toByteArray());
+
+        AsyncTask<InputStream, String, Face[]> detectTask =
+                new AsyncTask<InputStream, String, Face[]>() {
+                    String exceptionMessage = "";
+
+                    @Override
+                    protected Face[] doInBackground(InputStream... params) {
+                        try {
+                            publishProgress("Detecting...");
+                            Face[] result = faceServiceClient.detect(
+                                    params[0],
+                                    true,         // returnFaceId
+                                    false,        // returnFaceLandmarks
+                                    // returnFaceAttributes:
+                                 new FaceServiceClient.FaceAttributeType[] {
+                                    FaceServiceClient.FaceAttributeType.Emotion
+                                 });
+
+                            if (result == null){
+                                publishProgress(
+                                        "Detection Finished. Nothing detected");
+                                return null;
+                            }
+                            publishProgress(String.format(
+                                    "Detection Finished. %d face(s) detected",
+                                    result.length));
+                            return result;
+                        } catch (Exception e) {
+                            exceptionMessage = String.format(
+                                    "Detection failed: %s", e.getMessage());
+                            return null;
+                        }
+                    }
+
+                    @Override
+                    protected void onPreExecute() {
+                        //TODO: show progress dialog
+                        detectionProgressDialog.show();
+                    }
+                    @Override
+                    protected void onProgressUpdate(String... progress) {
+                        //TODO: update progress
+                        detectionProgressDialog.setMessage(progress[0]);
+                    }
+                    @Override
+                    protected void onPostExecute(Face[] result) {
+                        //TODO: update face frames
+                        detectionProgressDialog.dismiss();
+
+                        if(!exceptionMessage.equals("")){
+                            showError(exceptionMessage);
+                        }
+                        if (result == null) return;
+
+                        Log.i("emotion anger: ", new Double(result[0].faceAttributes.emotion.anger).toString());
+                        Log.i("emotion contempt: ", new Double(result[0].faceAttributes.emotion.contempt).toString());
+                        Log.i("emotion disguswt: ", new Double(result[0].faceAttributes.emotion.disgust).toString());
+                        Log.i("emotion fear: ", new Double(result[0].faceAttributes.emotion.fear).toString());
+                        Log.i("emotion happiness: ", new Double(result[0].faceAttributes.emotion.happiness).toString());
+                        Log.i("emotion neutral: ", new Double(result[0].faceAttributes.emotion.neutral).toString());
+                        Log.i("emotion sadness: ", new Double(result[0].faceAttributes.emotion.sadness).toString());
+                        Log.i("emotion surprise: ", new Double(result[0].faceAttributes.emotion.surprise).toString());
+
+                        ImageView imageView = findViewById(R.id.imageview);
+                        imageView.setImageBitmap(
+                                drawFaceRectanglesOnBitmap(imageBitmap, result));
+                        imageBitmap.recycle();
+                    }
+                };
+
+        detectTask.execute(inputStream);
+    }
+    private static Bitmap drawFaceRectanglesOnBitmap(
+            Bitmap originalBitmap, Face[] faces) {
+        Bitmap bitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(10);
+        if (faces != null) {
+            for (Face face : faces) {
+                FaceRectangle faceRectangle = face.faceRectangle;
+                canvas.drawRect(
+                        faceRectangle.left,
+                        faceRectangle.top,
+                        faceRectangle.left + faceRectangle.width,
+                        faceRectangle.top + faceRectangle.height,
+                        paint);
+            }
+        }
+        return bitmap;
+    }
+    private void showError(String message) {
+        new AlertDialog.Builder(this)
+                .setTitle("Error")
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }})
+                .create().show();
+    }
 
 }
